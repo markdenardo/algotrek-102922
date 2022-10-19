d2df723c84456aad84c6d111dcf40d12d72d275e
MDN <mark.denardo@gmail.com>
2022-10-18 21:55:08 -0400

    Create p5-spiral.js

---
+++ p5-spiral.js
@@ -0,0 +1,20 @@
+
+p1 = new P5()
+s0.init({src:p1.canvas})
+p1.draw=()=>{
+p1.background(0, 220, 204);
+	p1.fill(0, 25, 149);
+	p1.stroke(0);
+	
+	p1.translate(p1.width/2, p1.height/2);
+	
+	for (i=0; i < 100; i++){
+		p1.push();
+		p1.rotate(i*100 / 15.0);
+		p1.scale(i / 16.0);
+		p1.ellipse(i,i, 100, 100);
+		p1.pop();
+	}
+}
+src(s0).modulate(o0,()=>Math.sin(Math.random(Math.PI))).repeat(2,1).out(o0)
+render(o0)
