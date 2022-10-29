aea81f0df8f7969a48caa1fe9aa3e9e8196b8d21
MDN <mark.denardo@gmail.com>
2022-10-28 22:28:12 -0400

    Create SET.js

---
+++ SET.js
@@ -0,0 +1,22 @@
+// pillars
+shape(()=>time)
+.repeat(()=>time,1)
+  .kaleid(()=>Math.PI)
+  .scale(0.3,0.2)
+  .modulate(osc())
+.luma(1)
+  .add(o0,0.7)
+.repeat(()=>Math.PI,)
+.color(()=>Math.random(time))
+  .out(o3)
+// seance
+let x = 1
+let f = ()=> Math.sin(time*3) * (Math.random() < 0.06 ? (x^=1) : x)
+s0.initImage('https://raw.githubusercontent.com/markdenardo/hpp/main/halloween_pumpkin_person(0)_0.png')
+s1.initImage('https://raw.githubusercontent.com/markdenardo/hpp/main/halloween_pumpkin_person(0)_1.png')
+s2.initImage('https://raw.githubusercontent.com/markdenardo/hpp/main/halloween_pumpkin_person(0)_2.png')
+s3.initImage('https://raw.githubusercontent.com/markdenardo/hpp/main/halloween_pumpkin_person(0)_5.png')
+src(s0).out(o0)
+osc(10,0.1,10).modulate(osc(f,f,f),0.5).out(o1)
+src(o1).blend(o0,()=>Math.PI).out(o2)
+render()
